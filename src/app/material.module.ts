60391d64ed417ca2e8e2c147b88a891c47238700
Ruslan Zenin <ruslan.zenin@gmail.com>
2020-11-29 12:07:49 -0500

    material additions

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,21 @@
 import {NgModule} from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule
+  ]
 })
 export class MaterialModule {}
