831dbf2c7067cbc4f0305f4ea482ec4532cf62cc
Ruslan Zenin <ruslan.zenin@gmail.com>
2020-11-29 06:46:26 -0500

    added generated artifacts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'todos', pathMatch: 'full' },
+  { path: 'todos', component: TodoListComponent },
+  { path: 'add', component: AddTodoComponent },
+  { path: 'todos/:id', component: TodoDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
+import { TodoDetailsComponent } from './components/todo-details/todo-details.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
+    TodoDetailsComponent,
+    TodoListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
